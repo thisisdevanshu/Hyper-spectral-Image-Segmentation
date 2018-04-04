3719b50a512a7e129ef44f77928f9807ee794dfb
Devanshu Singh <devanshusingh@Devanshus-MacBook-Pro.local>
2018-04-04 02:54:21 -0400

    Connected Components implemented

---
+++ ConnectedComponent.m
@@ -0,0 +1,29 @@
+function[CCIm] = connectedComponents(clusterIm,clustNum,m,n)
+    CCIm = zeros(m*n,1);
+    %connected component count
+    count = 1;
+    for clust = 1:clustNum
+        bw = zeros(m*n,1);
+        % converting each clsuter to bw image
+        for itr = 1:size(clusterIm,1)
+            if clusterIm(itr) == clust
+                bw(itr) = 1;
+            end
+        end
+        %finding the connected componets
+        cc = bwconncomp(bw);
+        componentsFound = size(cc.PixelIdxList,2);
+        for itr = 1:componentsFound
+            pointsInComponent = size(cc.PixelIdxList{itr},1);
+            for jtr = 1:pointsInComponent
+                imageIndex = cc.PixelIdxList{itr}(jtr);
+                CCIm(imageIndex) = count; 
+            end
+            count = count + 1;
+        end
+        
+    end
+    
+    CCIm = reshape(CCIm,m,n);
+
+end
