cc22abf31a9ce5bd917152ed5e82d00fe34489a1
Devanshu Singh <devanshusingh@Devanshus-MacBook-Pro.local>
2018-04-04 00:56:56 -0400

    Hyperspectral images functional

--- SOM/SOM_RGB.m
+++ SOM/SOM_RGB.m
@@ -1,58 +1,50 @@
-function [ClusterIm, CCIm] = SOM(data,nodes,ImageType);
+function [] = SOM(data,nodes);
+R = data(:,:,1);
+G = data(:,:,2);
+B = data(:,:,3);
+R = double(R);
+G = double(G);
+B = double(B);
+R = R(:);
+G = G(:);
+B = B(:);
+summation = R+G+B;
+R = R./summation;
+G = G./summation;
+B = B./summation;
+data = [R,G,B];
 
-m = size(data,1);
-n = size(data,2);
-b = size(data,3);
-if(strcmp('RGB',ImageType)==1)
-    R = data(:,:,1);
-    G = data(:,:,2);
-    B = data(:,:,3);
-    R = double(R);
-    G = double(G);
-    B = double(B);
-    R = R(:);
-    G = G(:);
-    B = B(:);
-    %summation = R+G+B;
-    %R = R./summation;
-    %G = G./summation;
-    %B = B./summation;
-    data = [R,G,B];
-end
-if(strcmp('Hyper',ImageType)==1)
-    b = 20;
-    [Y, U, Lambda, Mu] = PCAbyDG(data,b);
-    data = Y;
-end
-size(data)
 totalW = nodes;
 %//initialization of weights
-w = rand(totalW,b).*256;
+wold = rand(totalW,3);
 %// the initial learning rate
-eta0 = 0.01;
+eta0 = 0.09;
 %// the current learning rate (updated every epoch)
 etaN = eta0;
 %// the constant for calculating learning rate
 tau2 = 1000;
 %creating map out of node
 [I,J] = ind2sub([sqrt(totalW),sqrt(totalW)], 1:totalW);
-%N = size(data,2);
+N = size(data,2);
 %// the size of neighbor
 sig0 = 2;
 sigN = sig0;
 %// tau 1 for updateing sigma
 tau1 = 1000/log(sigN);
-out = zeros(m*n,1);
+out = zeros(154401,1);
 %i is number of epoch
-%itr = 100;
-wold = w;
-for itr=1:10
+itr = 5;
+wnew = wold + 0.01
+count = 0
+while sum(abs(wnew - wold) > 1.0e-05*2) > 0
     %// j is index of each point.
     %// it should iterate through data in a random order rewrite!!
-    for j=1:m*n
+    count = count + 1
+    for j=1:154401   
+        wold = wnew;
         x = data(j,:);
         % 2 is for summing rows
-        dist = sum( sqrt((w - repmat(x,totalW,1)).^2),2);
+        dist = sum( sqrt((wold - repmat(x,totalW,1)).^2),2);
         %// find the winner
         [v ind] = min(dist);
         %// the 2-D index
@@ -62,16 +54,14 @@ for itr=1:10
         dist = 1/(sqrt(2*pi)*sigN).*exp( sum(( ([I( : ), J( : )] - repmat(ri, totalW,1)) .^2) ,2)/(-2*sigN)) * etaN;
         
         %// updating weights  
-        w = w + dist.*( x - w);
-   
-    end
+        wnew = wold + dist.*( x - wold);
+        
+    end    
+    abs(wnew - wold)
     %// update learning rate
-    etaN = eta0 * exp(-itr/tau2);
+    etaN = eta0 * exp(-count/tau2);
     %// update sigma
-    sigN = sig0*exp(-itr/tau1);
+    sigN = sig0*exp(-count/tau1);
 end
-%w-wold
-ClusterIm = out;
-im = reshape(out,m,n);
-im = medfilt2(im);
-imagesc( imgaussfilt(im,1) );
+count
+imagesc(reshape(out,321,481));
