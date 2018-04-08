69642c9268c2bba9eefe3e37e64987b625f272a0
karanchauhan2@gmail.com <karanchauhan2@gmail.com>
2018-04-08 23:30:25 +0000

    Added support for hyperspectral clustering

--- main_functions/TestImage9.m
+++ main_functions/TestImage9.m
@@ -5,11 +5,16 @@ matData = load('PaviaHyperIm.mat'); %use for RGB
 
 %Img = matData.Im; %use for RGB
 Img = matData.PaviaHyperIm; %use for Pavia
+GroundTruthMatData = load('PaviaGrTruth.mat');
+GroundTruth = GroundTruthMatData.PaviaGrTruth;
+MaskMatData = load('PaviaGrTruthMask.mat');
+Mask = MaskMatData.PaviaGrTruthMask;
 
-%[ClusterIm, CCIm] = MyClust9(Img,'Algorithm', 'Kmeans', 'ImType', 'Hyper', 'NumClusts', 9);
-[ClusterIm, CCIm] = MyClust9(Img,'Algorithm', 'Kmeans', 'ImType', 'RGB', 'NumClusts', 3);
+[ClusterIm, CCIm] = MyClust9(Img,'Algorithm', 'FCM', 'ImType', 'Hyper', 'NumClusts', 9);
+%[ClusterIm, CCIm] = MyClust9(Img,'Algorithm', 'Kmeans', 'ImType', 'RGB', 'NumClusts', 3);
 
 [m,n] = size(CCIm);
+index = MyClustEvalHyper9(ClusterIm,GroundTruth,Mask)
 ClusterIm = reshape(ClusterIm, [m,n]);
 
 imagesc(ClusterIm);
